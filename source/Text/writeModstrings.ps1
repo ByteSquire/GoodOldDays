7a326cb48dbcd57c25e193076a226e5d71b1eda0
ByteSquire <56401181+ByteSquire@users.noreply.github.com>
2021-11-12 22:03:02 +0100

    fix mastery names, tags, mastery panel positions

--- source/Text/writeModstrings.ps1
+++ source/Text/writeModstrings.ps1
@@ -1,28 +1,4 @@
-if ( $args.count -lt 1 ) { $path = "Text_EN" }
-elseif ( $args.count -eq 1 ) { $path = $args[0] }
-else {
-    Write-Host "Too many arguments"
-    Exit 1
-}
-
-if (!(Test-Path -Path $path)) {
-    Write-Host "Specified path not found"
-    Exit 1
-}
-
-Get-Childitem -Directory |
-
-ForEach-Object {
-    Write-Modstring($_.FullName)
-}
-
-$modstringpath = "modstrings.txt"
-if (Test-Path -Path $modstringpath) { Clear-Content -Path $modstringpath }
-else { New-Item -ItemType File $modstringpath }
-
-Add-Content -Path $modstringpath -Value (Get-Content $path\modstrings.txt)
-
-
+# function for language resource folder
 function Write-Modstring([string] $path) {
     $modstringpath = "$path\modstrings.txt"
     if (Test-Path -Path $modstringpath) { Clear-Content -Path $modstringpath }
@@ -51,3 +27,28 @@ function Write-Modstring([string] $path) {
     }
 
 }
+
+# main
+if ( $args.count -lt 1 ) { $path = "Text_EN" }
+elseif ( $args.count -eq 1 ) { $path = $args[0] }
+else {
+    Write-Host "Too many arguments"
+    Exit 1
+}
+
+if (!(Test-Path -Path $path)) {
+    Write-Host "Specified path not found"
+    Exit 1
+}
+
+Get-Childitem -Directory |
+
+ForEach-Object {
+    Write-Modstring($_.FullName)
+}
+
+$modstringpath = "modstrings.txt"
+if (Test-Path -Path $modstringpath) { Clear-Content -Path $modstringpath }
+else { New-Item -ItemType File $modstringpath }
+
+Add-Content -Path $modstringpath -Value (Get-Content $path\modstrings.txt)
